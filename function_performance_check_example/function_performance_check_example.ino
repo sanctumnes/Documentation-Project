d5563dfd7d9d89f7353499f73bc74bed6be666ba
sanctumnes <111234209+sanctumnes@users.noreply.github.com>
2022-12-13 18:46:50 +0200

    Add files via upload

--- function_performance_check_example/function_performance_check_example.ino
+++ function_performance_check_example/function_performance_check_example.ino
@@ -1,1 +1,38 @@
+void taskOneFunction() {
+  //some task, for demonstration we will use a for loop
+  for(int i=0;i<10;i++) {
+    delay(100);
+  }
+}
 
+void taskTwoFunction() {
+  //some task, for demonstration we will use a for loop
+  for(int i=0;i<10;i++) {
+    Serial.print("The current iteration is: ");
+    Serial.println(i);
+    delay(100);
+  }
+}
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int startTime1=millis();
+  taskOneFunction();
+  int endTime1_startTime2=millis();
+  taskTwoFunction();
+  int endTime2=millis();
+
+  Serial.print("Function one took this time to execute: ");
+  // the time to execute function one is the time when it finished minus when it started
+  Serial.println(endTime1_startTime2 - startTime1);
+  
+  Serial.print("Function two took this time to execute: ");
+  // the time to execute function two is the time when it finished minus when it started
+  Serial.println(endTime1_startTime2 - startTime1); 
+  
+  Serial.println(); // new line for tidy prints
+}
