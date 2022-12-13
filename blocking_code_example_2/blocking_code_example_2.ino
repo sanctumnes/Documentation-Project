b16a8e1e91ceb180d16069ef5741de58d77abcc3
sanctumnes <111234209+sanctumnes@users.noreply.github.com>
2022-12-13 18:45:38 +0200

    Add files via upload

--- blocking_code_example_2/blocking_code_example_2.ino
+++ blocking_code_example_2/blocking_code_example_2.ino
@@ -1,1 +1,12 @@
+void setup() {
+  Serial.begin(9600);
 
+}
+
+void loop() {
+  Serial.println("Hello!");
+  for(int i=0; i<1000; i++) {
+    Serial.println("This Loop Is A Blocking Code!");
+  }
+  Serial.println("Hello Again!");
+}
