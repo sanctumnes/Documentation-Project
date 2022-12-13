5617cabc34ba6597baa661f8c3d1d22f5c37425e
sanctumnes <111234209+sanctumnes@users.noreply.github.com>
2022-12-13 18:46:27 +0200

    Add files via upload

--- blocking_code_problem_example/blocking_code_problem_example.ino
+++ blocking_code_problem_example/blocking_code_problem_example.ino
@@ -1,1 +1,25 @@
+const int buttonPin = 5;
+bool flag = true;
+void setup() {
+  Serial.begin(9600);
+}
 
+void loop() {
+  if(flag) {
+    Serial.println("Executing Task 1 Every 5 Seconds");
+    /*
+    SOME CODE FOR TASK 1 
+    SOME CODE FOR TASK 1
+    SOME CODE FOR TASK 1
+    */
+    Serial.println("Task 1 Done");
+    delay(5000);
+  
+    if(digitalRead(buttonPin) == LOW) {
+      flag = false;
+    }  
+  }
+  else {
+    Serial.println("Task 1 Stopped Executing!");
+  }
+}
