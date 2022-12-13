9f4f6d63954c536b73de4c0b914366cd161c7320
sanctumnes <111234209+sanctumnes@users.noreply.github.com>
2022-12-13 18:48:03 +0200

    Add files via upload

--- timer_interrupt_blink_and_write_to_serial_monitor_every_second/timer_interrupt_blink_and_write_to_serial_monitor_every_second.ino
+++ timer_interrupt_blink_and_write_to_serial_monitor_every_second/timer_interrupt_blink_and_write_to_serial_monitor_every_second.ino
@@ -1,1 +1,44 @@
+volatile int interruptCounter;
+int totalInterruptCounter;
+ 
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
+int ledPin = 2;
+int state = LOW;
+ 
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  state=!state;
+  digitalWrite(ledPin,state);
+  portEXIT_CRITICAL_ISR(&timerMux);
+ 
+}
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmEnable(timer);
+ 
+}
+ 
+void loop() {
+ 
+  if (interruptCounter > 0) {
+ 
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+ 
+    totalInterruptCounter++;
+ 
+    Serial.print("An interrupt has occurred. Total number: ");
+    Serial.println(totalInterruptCounter);
+ 
+  }
+}
